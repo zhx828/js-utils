21e9d7d11a7c1559832a887822d44a80d0c5ebb9
Hongxin Zhang <jackson.zhang.828@gmail.com>
2015-09-01 22:27:47 -0400

    Add hammingWeight

---
+++ hammingWeight.js
@@ -0,0 +1,20 @@
+/**
+ * Created by zhangh2 on 9/1/15.
+ */
+/**
+ * @param {number} n - a positive integer
+ * @return {number}
+ */
+var hammingWeight = function(n) {
+    var count = 0;
+
+    if(n >   Math.pow(2, 31)) {
+        n ^= Math.pow(2, 31)
+        count++;
+    }
+    while(n > 0) {
+        n &= n-1;
+        count++;
+    }
+    return count;
+};
